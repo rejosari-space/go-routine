a2548f6d097f3d32285ea2a28857ffb734871058
rejosari-space <abdulazizmuslim.id@gmail.com>
2025-10-04 23:40:20 +0700

    rwmutex

--- goroutine/mutex_test.go
+++ goroutine/mutex_test.go
@@ -25,3 +25,42 @@ func TestMutex(t *testing.T) {
 	time.Sleep(5 * time.Second)
 	fmt.Println("value of x =>", x)
 }
+
+type BankAccount struct {
+	RWmutex sync.RWMutex
+	Balance int
+}
+
+// method to add balance
+func (account *BankAccount) AddBalance(amount int) {
+	account.RWmutex.Lock() //lock for writing
+	account.Balance += amount
+	account.RWmutex.Unlock() //unlock for writing
+}
+
+// method to get balance
+func (account *BankAccount) GetBalance() int {
+	account.RWmutex.RLock() //lock for reading
+	balance := account.Balance
+	account.RWmutex.RUnlock() //unlock for reading
+
+	return balance
+}
+
+func TestRwMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 1; i <= 100; i++ {
+		go func() {
+			for j := 1; j <= 100; j++ {
+
+				account.AddBalance(1)
+				fmt.Println("current balance", account.GetBalance())
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+
+	fmt.Println("final balance", account.GetBalance())
+}
